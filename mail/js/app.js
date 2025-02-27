92d3753304d28e79a2d059ba92a2a370cd9d26b6
alemalagni <alemalagni@gmail.com>
2025-02-27 16:20:55 +0100

    feat: dichiarazione lista

---
+++ mail/js/app.js
@@ -0,0 +1,9 @@
+// Dichiarare le variabili
+const lista = [
+    "giovanni@fiscali.it",
+    "paolo89@gnail.com",
+    "annamaria@mail.it",
+    "pippo@pippo.com",
+    "optimusprime@outfork.com",
+    "rickyone@gnail.com",
+]
