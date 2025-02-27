0d477330b2c6ca9273def4a986b1370176c1657d
alemalagni <alemalagni@gmail.com>
2025-02-27 16:24:49 +0100

    feat: add check script

--- mail/js/app.js
+++ mail/js/app.js
@@ -7,3 +7,14 @@ const lista = [
     "optimusprime@outfork.com",
     "rickyone@gnail.com",
 ]
+
+// Chiedere all'utente la mail
+const mail = prompt("Inserire la propria mail:");
+let controllo = false;
+
+// Controllo mail
+for( let i = 0; i < lista.length || controllo === true ; i++) {
+    if ( lista[i] === mail ) {
+        controllo = true;
+    }
+}
